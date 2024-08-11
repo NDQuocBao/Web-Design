d852d1aaae06b0ec8ee0de0b07a13c5ff0243dd0
NDQuocBao <baon6172@gmail.com>
2024-08-11 12:47:08 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,35 +1,37 @@
 
-const slides = document.querySelectorAll('.carousel-slide');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
+function init() {
+    const slides = document.querySelectorAll('.carousel-slide');
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
 
-let currentIndex = 0;
+    let currentIndex = 0;
 
-function showSlide(index) {
-    if (index < 0) {
-        currentIndex = slides.length - 1;
-    } else if (index >= slides.length) {
-        currentIndex = 0;
-    } else {
-        currentIndex = index;
+    function showSlide(index) {
+        if (index < 0) {
+            currentIndex = slides.length - 1;
+        } else if (index >= slides.length) {
+            currentIndex = 0;
+        } else {
+            currentIndex = index;
+        }
+
+        const newTransformValue = -currentIndex * 100;
+        document.querySelector('.carousel-container').style.transform = `translateX(${newTransformValue}%)`;
     }
 
-    const newTransformValue = -currentIndex * 100;
-    document.querySelector('.carousel-container').style.transform = `translateX(${newTransformValue}%)`;
+    nextButton.addEventListener('click', () => {
+        showSlide(currentIndex + 1);
+    });
+
+    prevButton.addEventListener('click', () => {
+        showSlide(currentIndex - 1);
+    });
+
+    setInterval(() => {
+        showSlide(currentIndex + 1);
+    }, 4000);
+
 }
-
-nextButton.addEventListener('click', () => {
-    showSlide(currentIndex + 1);
-});
-
-prevButton.addEventListener('click', () => {
-    showSlide(currentIndex - 1);
-});
-
-setInterval(() => {
-    showSlide(currentIndex + 1);
-}, 4000);
-
 
 window.onscroll = function () {
     let gototop = document.getElementById("gototop");
@@ -48,4 +50,7 @@ function goToTop() {
             clearInterval(timer);
     },10)   
 }
+
+
+
 
